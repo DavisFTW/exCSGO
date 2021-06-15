d1ba5a9de05efd739f8a58c3a17235a8bfc8a6f1
DavisFTW <31802124+DavisFTW@users.noreply.github.com>
2021-06-15 16:16:12 +0300

    Update Misc.cpp

--- Misc.cpp
+++ Misc.cpp
@@ -1,5 +1,6 @@
 #include "Misc.h"
 
+// UPGRADME: this check sometimes can fail due to source engine being slow, maybe a bette check would be if(localplayer) ? ? ? or see if the entity list is created ? 
 int misc::inGame(MemoryManager& obj)   // I should probably move this somewhere else so I don't have a 1 file for 1 function, but I have no idea where I could fit it in 
 {
 	DWORD enginePointer = memory::readMem<DWORD>(obj.getEngineDLL() + offsets::dwClientState, obj.getHandle());
